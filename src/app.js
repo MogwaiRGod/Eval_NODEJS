b28b978b2ca05516c8e1662ccee77b2ffb1b7da0
Diane <122520392+MogwaiRGod@users.noreply.github.com>
2023-02-09 11:58:40 +0100

    README + structure de l'API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+/************************************** IMPORT ET MONTAGE DES MODULES */
+
+const express = require("express"); // import d'express => module contenant des fonctionnalités 
+                                    // pour les applications web = pour les reqûetes HTTP =>
+                                    // récupération des fonctions :
+                                    // get(), use()...
+const app = express();  // notre API est une application express ; app est donc une instance d'express
+const fs = require('fs');   // module qui permet de manipuler des fichiers
+const bodyParser = require("body-parser"); // bodyParser est utile à express ; il lui permet de
+                                            // créer/lire/manipuler des données HTTP POST
+
+// app.use([chemin], [middleware]) -> cette fonction sert à monter un middleware (logiciel qui fournit des
+// fonctionnalités à une application) à un endroit spécifié ; si pas 
+// de chemin, alors monte le middleware entré en argument à la localisation actuelle
+app.use(bodyParser.json()); // montage de bodyParser
+
+
+/****************************************** VARIABLES */
+
+const chemin_menu = 'SRC/model/carte.json';    // chemin du fichier du menu
+
+
+// autorisation de l'export de app
 module.exports = app;
