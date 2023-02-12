a3d0cd532f90dd016906c56d87bde94418786932
Diane <diane.roberg@gmail.com>
2023-02-13 00:13:47 +0100

    fin

--- src/controller/controller_plats.js
+++ src/controller/controller_plats.js
@@ -1,23 +1,253 @@
 /*********************************** VARIABLES */
 const menu = './src/model/menu.json';   // chemin du fichier du menu
-const erreurs = // messages de statuts de requêtes
-{   
-    // erreurs 404
-    "404_id": "Aucun objet trouvé avec cet id",
-    "404_nom": "Aucun objet trouvé avec ce nom",
-    // erreurs 500
-    "500_lecture": "Une erreur est survenue lors de la lecture des données",
-    "500_ecriture": "Une erreur est survenue lors de l'écriture des données",
-    // codes 200
-    "200_ajout": "Donnée ajoutée avec succès !",
-    "200_maj": "Donnée mise à jour avec succès !",
-    "200_suppr": "Donnée supprimée avec succès !",
-    // erreurs 400
-    "400_vide": "Corps de requête vide",
-    "400_deja_existant": "Il y a déjà un objet avec cet ID dans le tableau",
-    "400_invalide": "Propriété(s) invalide(s)"
-};
+
 
 /*********************************** IMPORTS */
 const fs = require('fs'); 
 const manip_files = require('../utils/manipulateFiles');
+
+
+/******************************************************** CRUD ******************************************************/
+
+/********************************* CREATE *****************************/
+
+exports.ajouterPlat = (requete, reponse) => {
+    // lecture des données
+    fs.readFile(menu, (erreur, donnees) => {
+        // si erreur
+        if(manip_files.casErreurs(erreur, reponse, "lecture")){
+            // message + fin
+            return;
+        } else { 
+            // stockage des propriétés de la requête
+            const liste_props = Object.getOwnPropertyNames(requete.body);
+            // vérification de l'intégrité de la requête 
+            // = que le corps n'est pas vide et que les propriétés sont correctes
+            if (manip_files.checkBodyAjout(liste_props, reponse)) {
+                // si non-intègres -> erreur + fin
+                return;
+            } else if (manip_files.checkValeurs(requete.body.prix, reponse)) {  // si les propriétés ont OK, on vérifie que les valeurs le sont également
+                return;
+            } else {
+                let id; let donnees_existantes = JSON.parse(donnees);
+                // sinon, on détermine l'ID du nouveau plat
+                // si le tableau est vide -> id = 0             // sinon calcule l'ID selon l'ID le plus élevé attribué
+                (!donnees_existantes.plats.length) ? id = 0 : id = manip_files.defineId(donnees_existantes.plats);
+                // création de l'objet
+                const item = manip_files.creerItem(id, requete.body.nom, requete.body.prix);
+                // ajout au tableau
+                donnees_existantes.plats.push(item);
+                // réécriture de la BDD
+                fs.writeFile(menu, JSON.stringify(donnees_existantes), (erreur_write) => {
+                    // si erreur
+                    if(manip_files.casErreurs(erreur_write, reponse, 'ecriture')){
+                        // message + fin
+                        return;
+                    } else { // si succès
+                       // message + fin
+                        manip_files.succesReq(reponse, 'ajout');
+                        return;
+                    }// FIN SI
+                }); // FIN WRITE FILE
+            }// FIN SI
+        } // FIN SI
+    }); // FIN READ FILE
+} // FIN AJOUTER 
+
+// fonction qui ajoute un plat à la BDD en spécifiant son ID
+exports.ajouterPlatId = (requete, reponse) => {
+    // lecture BDD
+    fs.readFile(menu, (erreur, donnees) => {
+        if (manip_files.casErreurs(erreur, reponse, 'lecture')){
+            return;
+        } else {
+            const liste_props = Object.getOwnPropertyNames(requete.body);
+            let donnees_existantes = JSON.parse(donnees);
+            if (manip_files.checkId(requete.params.id, donnees_existantes.plats, reponse)) { // vérification que l'ID demandé n'est pas 
+                                                                                                // déjà attribué
+                // sinon erreur + fin
+                return;
+            } else if (manip_files.checkBodyAjout(liste_props, reponse)) { 
+                // vérificatoin de l'intégrité des données à entrer ; si non-intègres : erreur + fin
+                return;
+            } else if (manip_files.checkValeurs(requete.body.prix, reponse)) {  // si les propriétés ont OK, on vérifie que les valeurs le sont également
+                return;
+            } else {
+                // détermination de l'ID selon que le tableau est vide ou non
+                let id;
+                (!donnees_existantes.plats.length) ? id=0 : id=manip_files.defineId(donnees_existantes.plats);
+                // création de l'item
+                const item = manip_files.creerItem(id, requete.body.nom, requete.body.prix);
+                // ajout de l'item au tableau
+                donnees_existantes.plats.push(item);
+                // réécriture du fichier
+                fs.writeFile(menu, JSON.stringify(donnees_existantes), (erreur_write) => {
+                    // cas d'erreur
+                    if (manip_files.casErreurs(erreur_write, reponse, 'ecriture')) {
+                        return;
+                    } // cas de succès
+                    manip_files.succesReq(reponse, 'ajout');
+                });
+            }// FIN SI
+        } // FIN SI
+    }); // FIN READ FILE
+} // FIN AJOUTER ID
+
+
+
+/********************************* READ *****************************/
+// fonction permettant l'afficher tous les plats
+exports.afficherPlats = (requete, reponse) => {
+    // lecture du fichier de données
+    fs.readFile(menu, (erreur, donnees) => {
+        // si erreur
+        if (manip_files.casErreurs(erreur, reponse, 'lecture')) {
+            return;
+        } else {
+            // stockage des données demandées
+            const donnees_existantes = JSON.parse(donnees).plats;
+            // s'il n'y a pas de données
+            if (manip_files.checkTab(donnees_existantes, reponse)) {
+                return;
+            }
+            // sinon, affichage des données
+            manip_files.requeteStatut(200, donnees_existantes, reponse);
+        }
+    }); // FIN READ FILE
+} // FIN AFFICHER plats
+
+// fonction qui permet d'afficher un item selon son id
+exports.afficherPlatId = (requete, reponse) => {
+    // lecture du fichier de données
+    fs.readFile(menu, (erreur, donnees) => {
+        // si erreur
+        if (manip_files.casErreurs(erreur, reponse, 'lecture')) {
+            return;
+        } else {
+            // stockage des données demandées
+            const donnees_existantes = JSON.parse(donnees).plats;
+            // s'il n'y a pas de données
+            if (manip_files.checkTab(donnees_existantes, reponse)) {
+                return;
+            } else if (manip_files.existeId(requete.params.id, donnees_existantes, reponse)) { 
+                // vérification que l'item demandé existe ; si oui :
+                // on affiche l'item selon l'ID demandé
+                manip_files.afficherItemId(requete.params.id, donnees_existantes, reponse);
+            } else {
+                // sinon, si aucun item n'a été trouvé, un message d'erreur a été envoyé et maintenant on quitte la fonction
+                return;
+            }// FIN SI
+        } // FIN SI
+    }); // FIN READ FILE
+
+} // FIN AFFICHER DESSERT PAR ID
+
+// fonction qui recherche les plats dont le nom correspond à une expression entrée dans la requête
+exports.recherchePlats = (requete, reponse) => {
+    // lecture du fichier de données
+    fs.readFile(menu, (erreur, donnees) => {
+        // si erreur
+        if (manip_files.casErreurs(erreur, reponse, 'lecture')) {
+            return;
+        } else {
+            // stockage du tableau
+            const donnees_existantes = JSON.parse(donnees).plats;
+            // vérification que le tableau est non-vide
+            if (manip_files.checkTab(donnees_existantes, reponse)) {
+                // si non, errerur + on quitte
+                return;
+            } else {
+                // on cherche dans le tableau les occurrences correspondantes
+                // si on en trouve, la fonction les affiche, sinon, message d'erreur
+                manip_files.rechercheItem(donnees_existantes, "nom", requete.params.recherche, reponse);
+                return;
+            }// FIN SI           
+        }// FIN SI
+    }); // FIN READ FILE
+} // FIN RECHERCHE 
+
+
+
+/********************************* UPDATE *****************************/
+
+exports.udpatePlat = (requete, reponse) => {
+    // lecture du fichier de données
+    fs.readFile(menu, (erreur, donnees) => {
+        // cas d'erreur
+        if (manip_files.casErreurs(erreur, reponse, 'lecture')){
+            return;
+        } else {
+            // sélection des données
+            let donnees_existantes = JSON.parse(donnees);
+            // vérification que le tableau est non-vide
+            if (manip_files.checkTab(donnees_existantes.plats, reponse)) {
+                return;
+            } else if (manip_files.existeId(requete.params.id, donnees_existantes.plats, reponse)) {
+                // vérification que l'item existe
+                // sélection des propriétés dans le corps de requête
+                const liste_props = Object.getOwnPropertyNames(requete.body);
+                // vérification que les propriétés demandées sont correctes
+                if (manip_files.checkPropsUpdate(liste_props, reponse)) {
+                    return;
+                } else if (liste_props.find(p => p.toString().toLowerCase() === "prix") && manip_files.checkValeurs(requete.body.prix, reponse)) {  // si les propriétés ont OK, on vérifie que les valeurs le sont également
+                    return;
+                } else {
+                    // on sélectionne l'item dans le tableau
+                    let item = donnees_existantes.plats.find( e => e.id === parseInt(requete.params.id));
+                    console.log(item)
+                    // on boucle dans les propriétés
+                    liste_props.forEach( p => {
+                        // on màj l'item selon elles
+                        item[p] = requete.body[p];
+                    }); // FIN FOR EACH
+                    // // réécriture du fichier de données
+                    fs.writeFile(menu, JSON.stringify(donnees_existantes), (erreur_write) => {
+                        // si erreur
+                        if (manip_files.casErreurs(erreur_write, reponse, 'ecriture')){
+                            return;
+                        } else {
+                            manip_files.succesReq(reponse, 'maj');
+                            return;
+                        }
+                    }); // FIN WRITE FILE
+                } // FI SI
+            } // FIN SI
+            return;
+        } // FIN SI
+    }); // FIN WRITE FILE
+} // FIN UPDATE 
+
+
+
+/********************************* DELETE *****************************/
+
+exports.supprimerPlat = (requete, reponse) => {
+    // lecture du fichier de données
+    fs.readFile(menu, (erreur, donnees) => {
+        // cas d'erreur
+        if (manip_files.casErreurs(erreur, reponse, 'lecture')) {
+            return;
+        } else {
+            // sélection des données
+            let donnees_existantes = JSON.parse(donnees);
+            // vérification que le tableau de plats n'est pas vide
+            if (manip_files.checkTab(donnees_existantes.plats, reponse)){
+                return;
+            } else if (manip_files.existeId(requete.params.id, donnees_existantes.plats, reponse)) {
+                // vérification que l'item demandé existe 
+                // si oui, on cherche son index dans le tableau
+                const index = donnees_existantes.plats.findIndex(obj => obj.id === parseInt(requete.params.id));
+                // suppression de l'item
+                donnees_existantes.plats.splice(index, 1);
+                // on réécrit les données
+                fs.writeFile(menu, JSON.stringify(donnees_existantes), (erreur_write) => {
+                    // cas d'erreur
+                    if (manip_files.casErreurs(erreur_write, reponse, 'ecriture')) {
+                        return;
+                    } // cas de succès
+                    manip_files.succesReq(reponse, 'suppr');
+                }); // FIN WRITE FILE
+            } // FIN SI
+        } //FIN SI
+    }); // FIN READ FILE
+} // FIN SUPPRIMER 
--- src/routes/routes_plats.js
+++ src/routes/routes_plats.js
@@ -12,7 +12,50 @@ const router_plats = express.Router();
 
 /********************************************** REQUÊTES */
 
+/************************************************* POST **********************************/
+// routes pour ajouter un Plat avec calcul automatique de l'ID
+// e.g http://localhost:3000/plats
+router_plats.post('/plats', controller_plats.ajouterPlat);
+router_plats.post('/plat', controller_plats.ajouterPlat);   // deuxième route sans "s" à Plat
 
+// routes pour ajouter un Plat en précisant l'ID dans la requête
+// e.g http://localhost:3000/plats/1
+router_plats.post('/plats/:id', controller_plats.ajouterPlatId);
+router_plats.post('/plat/:id', controller_plats.ajouterPlatId);
+
+
+
+/************************************************* GET ***************************************/
+// routes pour afficher tous les plats
+// e.g http://localhost:3000/plats
+router_plats.get('/plats', controller_plats.afficherPlats);
+router_plats.get('/plat', controller_plats.afficherPlats);
+
+// route pour afficher un Plat spécifique selon son ID
+// e.g http://localhost:3000/plats/1
+router_plats.get('/plats/:id', controller_plats.afficherPlatId);
+router_plats.get('/plat/:id', controller_plats.afficherPlatId);
+
+// route pour afficher tous les plats correspondants à une recherche de nom dans la requete
+// e.g http://localhost:3000/plats/search/safran
+router_plats.get('/plats/search/:recherche', controller_plats.recherchePlats);
+router_plats.get('/plat/search/:recherche', controller_plats.recherchePlats);
+
+
+
+/************************************************* PUT ***************************************/
+// route pour mettre à jour un Plat selon son ID
+// e.g http://localhost:3000/plats
+router_plats.put('/plats/:id', controller_plats.udpatePlat);
+router_plats.put('/plat/:id', controller_plats.udpatePlat);
+
+
+
+/************************************************* DELETE ***************************************/
+// route pour supprimer un Plat selon son ID
+// e.g http://localhost:3000/plats
+router_plats.delete('/plats/:id', controller_plats.supprimerPlat);
+router_plats.delete('/plat/:id', controller_plats.supprimerPlat);
 
 
 /************************************* EXPORT */
