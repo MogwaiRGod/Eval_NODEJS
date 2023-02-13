80e97d87358dcb862990a909728fe2f416436107
Diane <122520392+MogwaiRGod@users.noreply.github.com>
2023-02-13 22:53:44 +0100

    Update manipulate_files.js

--- src/utils/manipulate_files.js
+++ src/utils/manipulate_files.js
@@ -1,15 +1,14 @@
 /*
 * VARIABLES
 */
-
 // messages de statuts de requêtes
 const error = 
 {   
-    // error 404
+    // erreur 404
     "404_id": "Aucun objet trouvé avec cet id",
     "404_nom": "Aucun objet trouvé avec ce nom",
     "404_vide": "Le jeu de données demandé est vide",
-    // error 500
+    // erreur 500
     "500_lecture": "Une erreur est survenue lors de la lecture des données",
     "500_ecriture": "Une erreur est survenue lors de l'écriture des données",
     "500": "Erreur interne",
@@ -18,7 +17,7 @@ const error =
     "200_maj": "Donnée mise à jour avec succès !",
     "200_suppr": "Donnée supprimée avec succès !",
     "200": "La requête est un succès",
-    // error 400
+    // erreur 400
     "400_vide": "Corps de requête vide",
     "400_deja_existant": "Il y a déjà un objet avec cet ID dans le arrayleau",
     "400_invalide": "Propriété(s) invalide(s)",
@@ -35,14 +34,14 @@ const error =
 /*
 * POUR LES RESULTATS
 * DE REQUÊTES
-* <=> error/STATUTS ET MESSAGES
+* <=> ERREURS/STATUTS ET MESSAGES
 */
 // fonction qui envoie un code HTTP et un message à la suite d'une requête HTTP
 exports.requestStatus = (code, msg, resp) => {
     resp.status(parseInt(code)).send(msg);
 }
 
-// fonction qui vérifie si une erreur système s'est produite et le cas échant, envoie un erreur + message et retourne VRAI;
+// fonction qui vérifie si une erreur système s'est produite et le cas échant, envoie une erreur + message et retourne VRAI;
 // sinon ne fait rien et retourne FAUX
 exports.caseError = (error_sys, resp, type = "") => {
     // le type d'erreur est soit une erreur d'écriture, soit de lecture ; si à l'avenir,
@@ -81,7 +80,7 @@ exports.checkArray = (array, resp) => {
         return true;
     }
     return false;
-} // FIN CHECK array
+} // FIN CHECK ARRAY
 
 // fonction qui vérifie si un corps de requête est vide ou non ; si oui, envoie une erreur et retourne VRAI, sinon,
 // ne fait rien et retourne FAUX
@@ -91,7 +90,7 @@ exports.checkEmpty = (props, resp) => {
         return true;
     }
     return false;
-} // FIN CHECK VIDE
+} // FIN CHECK EMPTY
 
 // fonction qui vérifie l'intégrité des propriétés ; si elles sont ok, fonction ne fait rien et retourne FAUX ; sinon, message
 // d'erreur et retourne VRAI
@@ -103,9 +102,9 @@ exports.checkProperties = (props, resp) => {
         return true;
     }
     return false;
-} // FIN CHECK PRORIETES
+} // FIN CHECK PROPERTIES
 
-// fonction qui vérifie que les valeurs entrées dans le corps de la requête soient correctes
+// fonction qui vérifie que les valeurs entrées dans le corps de la requête sont correctes
 // concrètement, vérifie que le prix entré est bien un nombre ; si oui, retourne FAUX et ne fait rien ; si non, envoie une erreur et retourne VRAI
 exports.checkValues = (prix, resp) => {
     if (parseFloat(prix) != prix) {
@@ -113,11 +112,11 @@ exports.checkValues = (prix, resp) => {
         return true;
     }
     return false;
-} // FIN CHECK VALEURS
+} // FIN CHECK VALUES
 
 // fonction qui évalue une liste de propriétés ; fonction à utiliser dans le cas d'une mise à jour d'item. La fonction va vérifier
 // que l'on demande à modifier 1 à 2 propriétés et que les propriétés demandées correspondent au prix ou au nom de l'item
-// affiche un message d'erreur si les propriétés  sont incorrectes et retourne VRAI, sinon retourne FAUX
+// affiche un message d'erreur si les propriétés sont incorrectes et retourne VRAI, sinon retourne FAUX
 exports.checkPropsUpdate = (props, resp) => {
     // on vérifie que l'on demande à maximum 2 propriétés
     if (this.checkEmpty(props, resp)){
@@ -137,7 +136,7 @@ exports.checkPropsUpdate = (props, resp) => {
     });
     // sinon retourne faux
     return false;
-} // FIN CHECK PRORIETES
+} // FIN CHECK PROPS UPDATE
 
 // fonction qui vérifie tout dans le corps de la requête : qu'il est non-vide et que les propriétés et valeurs sont intègres pour l'ajout d'un item
 // si une erreur est détéctée, applique le statut d'erreur correspondant et retourne vrai
@@ -148,9 +147,9 @@ exports.checkBodyAdd = (props, resp) => {
         return true;
     }
     return false;
-} // FIN CHECKBODY
+} // FIN CHECK BODY ADD
 
-// fonction qui vérifie que l'id entré en argument n'est pas déjà pris par un objet du arrayleau passé en argument
+// fonction qui vérifie que l'id entré en argument n'est pas déjà pris par un objet du tableau passé en argument
 // si c'est le cas : message d'erreur + retourne vrai ; sinon ne ne fait rien et retourne faux
 exports.checkId = (id, array, resp) => {
     if(array.find(e => parseInt(e.id) === parseInt(id)) !== undefined){
@@ -160,7 +159,7 @@ exports.checkId = (id, array, resp) => {
     return false;
 } // FIN CHECK ID
 
-// fonction qui indique s'il existe déjà dans un arrayleau un item avec l'id demandé ;
+// fonction qui indique s'il existe déjà dans un tableau un item avec l'id demandé ;
 // si oui, ne fait rien et retourne VRAI, sinon envoie un message d'erreur et retourne FAUX
 exports.existsId = (id, array, resp) => {
     // s'il y a bien un item avec l'id demandé
@@ -218,28 +217,36 @@ exports.readItemId = (id, array, resp) => {
 // fonction qui cherche une expression (regex) dans une str de caractères (str)
 // retourne un booléen selon si la regex a été trouvé à l'intérieur ou non
 exports.searchRegex = (str, regex) => {
-    let regexFound = false;// indique si les caractères sont bons dans l'évaluation en cours
+    // indique si les caractères sont bons dans l'évaluation en cours
+    let regexFound = false;
 
     // standardisation des arguments
     str=str.toString().toLowerCase(); regex=regex.toString().toLowerCase();
 
-    // on boucle dans la str tant que la regex peut être contenu (en longueur) dans la chaîne
+    // on boucle dans la chaîne str tant que la regex peut être contenue (en longueur) à l'intérieur
     for (let i=0; i<= str.length-regex.length; i++) {
         // on boucle dans la regex afin d'évaluer les caractères de la chaîne et de la regex
         for (let j=0; j<regex.length; j++) {
             // si le caractère évalué n'est pas le même
             if (regex[j] !== str[i+j]) {
                 regexFound = false; // màj du booléen regexFound
-                if (j>0) { // si on a déjà trouvé des caractères identiques
-                    str=str.substr(i+j, str.length); // on enlève tous les caractères de la str évalués jusque-là
-                    return this.searchRegex(str, regex); // et on relance la fonction
+                // si on a déjà trouvé des caractères identiques
+                if (j>0) { 
+                    // on enlève tous les caractères de la str évalués jusque-là
+                    str=str.substr(i+j, str.length); 
+                    // et on relance la fonction avec this pour indiquer que la fonction a pour source ce fichier (car on va importer les fonctions par la suite)
+                    return this.searchRegex(str, regex); 
                 } else {
-                    break; // sinon, on se contente de sortir de la boucle pour itérer sur le prochain caractère de la chaîne
+                    // sinon, on se contente de sortir de la boucle pour itérer sur le prochain caractère de la chaîne
+                    break; 
                 } // FIN SI
-            } else if (j === regex.length-1) { // si on a évalué le dernier caractère de la regex (et qu'il est identique
-                regexFound = true;            // à celui en cours de la chaîne)
-                return regexFound; // on peut quitter la fonction
-            } else { // sinon, si le caractère évalué est le même
+            // si on a évalué le dernier caractère de la regex (et qu'il est identique à celui en cours de la chaîne)
+            } else if (j === regex.length-1) { 
+                regexFound = true;
+                // on peut quitter la fonction
+                return regexFound; 
+            } else { 
+                // sinon, si le caractère évalué est le même
                 regexFound = true;
             }// FIN SI
         } // FIN POUR J
@@ -250,7 +257,7 @@ exports.searchRegex = (str, regex) => {
 // fonction qui, pour une liste d'objets (array), une propriété spécifique à évaluer (prop) et une expression à rechercher (regex),
 // retourne une une response de requête affichant une liste de tous les items contenant la regex dans la valeur de la propriété susmentionnée,
 // ou un message d'erreur si rien n'a été trouvé
-// usage concret : pour un arrayleau de la BDD, va chercher tous les items dont le nom contient l'expression entrée dans la requête
+// usage concret : pour un tableau de la BDD, va chercher tous les items dont le nom contient l'expression entrée dans la requête
 exports.searchItem = (array, prop, regex, resp) => {
     let foundItems = [];
     // on boucle dans le arrayleau
@@ -270,13 +277,3 @@ exports.searchItem = (array, prop, regex, resp) => {
     return this.requestStatus(200, foundItems, resp);
      // sinon on affiche la liste des items ayant la regex dans leur nom
 } // FIN RECHERCHE ITEM
-
-
-
- 
-
-
-
-
-
-
