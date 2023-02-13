6cb3ee38d891ee84d9a7eb09ae8c889bdcab17e7
Diane <122520392+MogwaiRGod@users.noreply.github.com>
2023-02-13 22:48:01 +0100

    Update routes_starters.js

--- src/routes/routes_starters.js
+++ src/routes/routes_starters.js
@@ -4,14 +4,20 @@
 // express
 const express = require("express"); // import du module express 
 
+
 /*
 * VARIABLES
 */
-// Router est une classe d'express => express.Router() crée une instance de cette classe.
-// Cette classe permet de créer un middleware, ce middleware étant un gestionnaire de routes complet.
-const routerStarters = express.Router(); // création d'un router pour le tableau entrées
-                                        //  = fichier de stockage de toutes les routes
-const controllerStarters = require("../controller/controller_starters"); // import des fonctions du controller des entrées
+
+/*
+* Router est une classe d'express => express.Router() crée une instance de cette classe.
+* Cette classe permet de créer un middleware, ce middleware étant un gestionnaire de routes complet.
+*/
+// création d'un router pour le tableau starters
+//  = fichier de stockage de toutes les routes
+const routerStarters = express.Router();
+// import des fonctions du controller des entrées (starters)
+const controllerStarters = require("../controller/controller_starters"); 
 
 
 
@@ -23,14 +29,14 @@ const controllerStarters = require("../controller/controller_starters"); // impo
 /*
 * POST
 */
-// route pour créer une donnée
+// routes pour créer une donnée
 // e.g http://localhost:3000/starters
-routerStarters.post('/starters', controllerStarters.addStarter); // on sélectionne la fonction addStarter du fichier
-                                                                // importé dans controllerStarters
-                                                                // et on l'utilise comme fonction callback de la requête
+// ici, on sélectionne la fonction addStarter du fichier
+// importée dans controllerStarters et on l'utilise comme fonction callback de la requête
+routerStarters.post('/starters', controllerStarters.addStarter); 
 routerStarters.post('/starter', controllerStarters.addStarter);
 
-// route pour créer une données avec une id spécifique
+// routes pour créer une données avec une id spécifique
 // e.g http://localhost:3000/starters/1
 routerStarters.post('/starters/:id', controllerStarters.addStarterId);
 routerStarters.post('/starter/:id', controllerStarters.addStarterId);
@@ -39,17 +45,17 @@ routerStarters.post('/starter/:id', controllerStarters.addStarterId);
 /*
 * GET
 */
-// route pour lire l'intégralité des entrées
+// routes pour lire l'intégralité des entrées
 // e.g http://localhost:3000/starters
 routerStarters.get('/starters', controllerStarters.readStarters);
 routerStarters.get('/starter', controllerStarters.readStarters);
 
-// route pour lire une entrée selon son id
+// routes pour lire une entrée selon son id
 // e.g http://localhost:3000/starters/1
 routerStarters.get('/starters/:id', controllerStarters.readStarterId);
 routerStarters.get('/starter/:id', controllerStarters.readStarterId);
 
-// route pour afficher une entrée selon son nom
+// routes pour afficher une entrée selon son nom
 // e.g http://localhost:3000/starters/search/croquette
 routerStarters.get('/starters/search/:name', controllerStarters.searchStarter);
 routerStarters.get('/starter/search/:name', controllerStarters.searchStarter);
@@ -58,7 +64,7 @@ routerStarters.get('/starter/search/:name', controllerStarters.searchStarter);
 /*
 * PUT
 */
-// route pour modifier une donnée (selon son id)
+// routes pour modifier une donnée (selon son id)
 // e.g http://localhost:3000/1
 routerStarters.put('/starters/:id', controllerStarters.updateStarter);
 routerStarters.put('/starter/:id', controllerStarters.updateStarter);
@@ -67,7 +73,7 @@ routerStarters.put('/starter/:id', controllerStarters.updateStarter);
 /*
 * DELETE
 */
-// route pour supprimer une donnée (selon son id)
+// routes pour supprimer une donnée (selon son id)
 // e.g http://localhost:3000/starters/1
 routerStarters.delete('/starters/:id', controllerStarters.deleteStarter);
 routerStarters.delete('/starter/:id', controllerStarters.deleteStarter);
