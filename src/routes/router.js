f6d256a2a99c03527a510734dcb3bff0f4143118
Diane <122520392+MogwaiRGod@users.noreply.github.com>
2023-02-13 18:58:47 +0100

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,17 @@
-/**************************************************** IMPORTS ***************************************************************************/
+/*
+* IMPORTS
+*/
 
-/*************************************************** modules *******************/
+/*
+* modules
+*/
 const express = require("express");
 const bodyParser = require("body-parser"); 
 
-/*************************************************** routes *******************/
+
+/*
+* routers
+*/
 
 // import de tous les routers spécifiques aux tableaux
 const routes_entrees = require('./routes_entrees.js')
@@ -15,24 +22,34 @@ const routes_petits_dejeuners = require('./routes_petitsdejeuners.js')
 const routes_plats = require('./routes_plats.js')
 
 
-/**************************************************** VARIABLES ******************************************************************/
-
-const router = express.Router(); // instanciation du router
+/*
+* VARIABLES
+*/
+// instanciation du router
+const router = express.Router(); 
 router.use(bodyParser.json());
 
 
+/*
+* ROUTER
+*/
 
-/********************************************************** ROUTER **********************************************************************************************/
 
-/************************************************* test ***********************************/
-// test de la route menant à la racine
-// e.g http://localhost:3000/
+/*
+* test
+*
+* test de la route menant à la racine
+* e.g http://localhost:3000/
+*/
 router.get('/', (requete, reponse) => {
     reponse.status(200).send("La route est fonctionelle");
 });
 
-/************************************ routes **************************************************/
-// on utilise chaque router importé
+/* 
+* routes
+*
+* on utilise chaque router importé
+*/
 router.use(routes_entrees); 
 router.use(routes_boissons); 
 router.use(routes_desserts); 
@@ -42,6 +59,7 @@ router.use(routes_plats);
 
 
 
-/********************************** EXPORT *************************************/
-
+/*
+* EXPORT
+*/
 module.exports = router;
