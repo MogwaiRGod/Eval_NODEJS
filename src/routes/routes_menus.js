61e991c0c26774929fdc1094ae9120c840fc19ad
Diane <122520392+MogwaiRGod@users.noreply.github.com>
2023-02-13 19:06:56 +0100

    Update routes_menus.js

--- src/routes/routes_menus.js
+++ src/routes/routes_menus.js
@@ -1,21 +1,34 @@
-/****************************************************** IMPORTS ****************************************************/
+/* 
+* NOTE : pour chaque route, existe en deux versions : 
+* une avec un S à menu et une sans (pour pouvoir effectuer les requêtes 
+* sans se préoccuper du pluriel) 
+*
+*/
 
+/*
+* IMPORTS
+*/
+// express
 const express = require("express"); 
+// controller correspondant
 const controller_menus = require("../controller/controller_menus");
 
-
-
-/****************************************************** VARIABLES****************************************************/
+/*
+* VARIABLES
+*/
 
 const router_menus = express.Router(); // instanciation du router
 
 
 
-/*************************************************************** REQUÊTES ****************************************************/
+/*
+* REQUÊTES
+*/
 
-// pour chaque route, existe en deux versions : uns avec un S à menu et une sans (pour pouvoir effectuer les requêtes sans se préoccuper du pluriel)
 
-/************************************* POST ***********************************/
+/*
+* POST
+*/
 // routes permettant d'ajouter un menu à la BDD sans préciser d'ID
 // e.g http://localhost:3000/menus
 router_menus.post('/menus', controller_menus.ajouterMenu);
@@ -27,8 +40,10 @@ router_menus.post('/menus/:id', controller_menus.ajouterMenuId);
 router_menus.post('/menu/:id', controller_menus.ajouterMenuId);
 
 
-/************************************* GET ***********************************/
 
+/*
+* GET
+*/
 // routes qui permettent d'afficher le menu de la BDD
 // e.g http://localhost:3000/menus
 router_menus.get('/menus', controller_menus.afficherMenus);
@@ -45,21 +60,27 @@ router_menus.get('/menus/search/:recherche', controller_menus.chercherMenu);
 router_menus.get('/menu/search/:recherche', controller_menus.chercherMenu);
 
 
-/************************************* PUT ***********************************/
-
+/*
+* PUT
+*/
 // routes permettant de màj un menu
 // e.g http://localhost:3000/menus/1
 router_menus.put('/menus/:id', controller_menus.updateMenu);
 router_menus.put('/menu/:id', controller_menus.updateMenu);
 
 
-/************************************* DELETE ***********************************/
-
+/*
+* DELETE
+*/
 // routes permettant de supprimer un menu
 // e.g http://localhost:3000/menus/1
 router_menus.delete('/menus/:id', controller_menus.supprMenu);
 router_menus.delete('/menu/:id', controller_menus.supprMenu);
 
-/************************************* EXPORT ***********************************/
+
+
+/*
+* EXPORT
+*/
 // export du router
 module.exports = router_menus;
