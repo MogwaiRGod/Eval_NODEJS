50ecb690711c3a9a67a6be620fb6f524f2b3a8ac
Diane <122520392+MogwaiRGod@users.noreply.github.com>
2023-02-13 10:12:59 +0100

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -1,5 +1,5 @@
 const api = require('./app.js');
-const port = 4000; // choix du port sur lequel faire tourner l'application
+const port = 3000; // choix du port sur lequel faire tourner l'application
 
 // listen() va requêter le port entré en argument pour faire tourner API
 api.listen(port, () => {    // lancement de l'application
